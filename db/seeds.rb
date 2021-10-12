cbe436033d5e9f6ba0c0b337c1bec3deb5704262
AnnHo01 <punri1304@gmail.com>
2021-10-11 21:26:20 -0500

    Add 676 products to Product db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Faker::Name.unique.clear # Clears used values for Faker::Name
+Faker::UniqueGenerator.clear # Clears used value for all generators
+
+Product.delete_all
+
+676.times do
+  product = Product.create(
+    title: Faker::Name.name,
+    price: Faker::Commerce.price,
+    stock_quantity: Faker::Number.number
+  )
+end
+
+puts "Create #{Product.count} products."
